e4d9016816cce136550658c331c1f11af2b54be0
Abhishek Dubey <abhishekdubeycsp@gmail.com>
2026-09-24 16:49:08 +0530

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -4,18 +4,21 @@ module.exports.renderSignupForm=(req, res) => {
   res.render("users/signup.ejs");
 };
 
-module.exports.signup=async (req, res) => {
+module.exports.signup=async (req, res, next) => {
     const { username, email, password } = req.body;
     const newUser = new User({ email, username });
     const registeredUser = await User.register(newUser, password);
-    req.login(registeredUser, (err) => {
+      req.login(registeredUser, (err) => {
       if (err) {
         return res.status(500).send("Unable to log in after signup");
       }
       req.flash("success", "Welcome to WanderLust!");
-      const redirectUrl = res.locals.redirectUrl || "/listings";
+        const redirectUrl = res.locals.redirectUrl || "/listings";
       delete req.session.redirectUrl;
-      res.redirect(redirectUrl);
+        req.session.save((saveError) => {
+          if (saveError) return next(saveError);
+          res.redirect(redirectUrl);
+        });
     });
   }
 
@@ -30,7 +33,10 @@ module.exports.signup=async (req, res) => {
     req.flash("success", "Welcome back to WanderLust!");
     const redirectUrl = res.locals.redirectUrl || "/listings";
     delete req.session.redirectUrl;
-    res.redirect(redirectUrl);
+    req.session.save((saveError) => {
+      if (saveError) return res.status(500).send("Unable to save login session");
+      res.redirect(redirectUrl);
+    });
   };
 
 
@@ -39,7 +45,10 @@ module.exports.logout=(req, res, next) => {
     if (err) {
       return next(err);
     }
-    req.flash("success", "you are logged out!");
-    res.redirect("/listings");
+    req.session.destroy((sessionError) => {
+      if (sessionError) return next(sessionError);
+      res.clearCookie("connect.sid");
+      res.redirect("/listings");
+    });
   });
 };
